e2fa6a9eed7bacb985a1c7e5c67d4c1172eeed89
zzz <865077695@qq.com>
2017-06-20 12:02:10 +0800

    使用node+express+mongodb写博客系统开始

--- note3/index.js
+++ note3/index.js
@@ -2,3 +2,17 @@
  * Created by zzq on 2017/6/16.
  */
 
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello,express!')
+});
+
+app.get('/users/:name', function (req, res) {
+    if (req.query){
+        name = req.query.name || ''
+    }
+    res.send('Hello, '+req.params.name+name);
+})
+app.listen(3000)
